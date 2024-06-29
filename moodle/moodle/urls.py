da8f87ac2e12664ced41acb0e2e9d34229984893
duc098182 <lienminh0149@gmail.com>
2024-06-29 18:34:43 +0700

    Login page and Forgot page ver1

--- moodle/moodle/urls.py
+++ moodle/moodle/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for moodle project.
+URL configuration for MyProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('users.urls')),
+    path('home/', views.home, name="home"),
 ]
---
+++ moodle/users/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('user')
+        password = request.POST.get('pass')
+
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            messages.warning(request, 'Username or Password is incorrect.')
+            return redirect('login')
+        else:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'users/Login.html')
+
+def resetP(request):
+    if request.method == "POST":
+        username = request.POST.get('user')
+        password1 = request.POST.get('pass1')
+        password2 = request.POST.get('pass2')
+
+        user_exist = User.objects.filter(username=username).exists()
+        if(password1 != password2 or not user_exist):
+            messages.warning(request, 'Password do not match or Wrong Username.')
+            return redirect('resetPass')
+        else:
+            user = User.objects.get(username=username)
+            user.set_password(password2)
+            user.save()
+            messages.success(request, 'Successfully change password for: ' + username + '.')
+            return redirect('login')
+
+    return render(request, 'users/Forgot.html')
