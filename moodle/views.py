6e5d4547553892109df3e40a427b0cd42ba20d4e
hauvanvn <nvhau22@clc.fitus.edu.vn>
2024-08-23 15:44:44 +0700

    Add assignment event in home page

--- moodle/views.py
+++ moodle/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from courses.models import CourseClass as Class, Notification
+from courses.models import CourseClass as Class, Assignment
 from .nav_infomation import getIn4
+
+import datetime
 
 @login_required(login_url="users:login")
 def home(request):
@@ -10,12 +12,17 @@ def home(request):
 
     if Class.objects.exists():
         courses_temp = Class.objects.filter(participants=user.id).order_by('-date_created')
+
+        ev = Assignment.objects.filter(ForClass__in=courses_temp).order_by('date_opened', 'date_opened')
+        assignment = min(ev, key=lambda date: abs(date.date_closed - datetime.datetime.now(datetime.timezone.utc)))
+
         for course in courses_temp:
             teacher = course.participants.filter(is_staff=1)
             courses.append({'course': course, 'teacher': teacher})
     
 
-    return render(request, 'View_home.html', {'user': user, 'notifies' : notifications, 'events': events, 'courses' : courses})
+    return render(request, 'View_home.html', {'user': user, 'notifies' : notifications, 'events': events, 
+                                              'assignment' : assignment, 'courses' : courses})
 
 def Http404NotFound(request):
     return render(request, '404.html')
