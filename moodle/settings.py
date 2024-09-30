08d08de1523b3cbda91f616a14e0e3be64af68c9
duc098182 <lienminh0149@gmail.com>
2024-09-30 23:16:49 +0700

    Deleting private info

--- moodle/settings.py
+++ moodle/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-a6i^=^p#m9-run69&+1uk)91yge6j_mpw67k_(@kj$$1h8nfyf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'vanduc01032004@gmail.com'
-EMAIL_HOST_PASSWORD = 'imfr rair xqch vwza'
+EMAIL_HOST_USER = '' #Email to send messesges
+EMAIL_HOST_PASSWORD = '' #Password to allow django accessing the email
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
